830ab886c2aa096de54586e1932416181668f0a0
Dan Bright <dan@uplandsdynamic.com>
2024-06-18 10:45:15 +0100

    Remove superflous code

--- user_prompt_end_v1.py
+++
@@ -1,4 +0,0 @@
-USER_PROMPTS_END = [{
-    "role": "user",
-    "content": "Take a deep breath and solve the problem step by step. It's important you get this right.",
-}]
--- user_prompt_end_v2.py
+++
@@ -1,4 +0,0 @@
-USER_PROMPTS_END = [{
-    "role": "user",
-    "content": "Take the time to solve the problem step by step. It is vital that the results are accurate. Prioritise accuracy over speed.",
-}]
